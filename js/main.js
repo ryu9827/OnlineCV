f5b1f296842b17d81f76a5163153ec141c5ebc2a
Bruce Li <ryu9827@gmail.com>
2017-11-16 14:26:11 +1300

    My Online Resume. www.brucelihunting4ajob.info

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+   
+var myName = "Bruce";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];//[196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, green, blue, purple, orange];
+
+
+
+drawName(myName, letterColors);
+
+if(5 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
